74c2c4659d99fcca8e8c02cd8e5b9069229ab156
Toru <toru.inoue@kissaki.tv>
2014-08-15 00:38:29 +0900

    再帰足した

--- fac.erl
+++ fac.erl
@@ -3,3 +3,11 @@
 
 fac(0) -> 1;
 fac(N) -> N * fac(N-1).
+
+len([]) -> 0;
+len([_|T]) -> 1 + len(T).
+
+tail_fac(N) -> tail_fac(N, 1).
+
+tail_fac(0, Rest) -> Rest;
+tail_fac(N, Last) when 0 < N -> tail_fac(N - 1, N * Last).
